c7862a9cfd3abb638440a48ee1cb056057d524df
erzaf98 <86522946+erzaf98@users.noreply.github.com>
2021-06-26 23:30:55 +0700

    Add files via upload

---
+++ webg3.js
@@ -0,0 +1,122 @@
+
+$(document).ready(function(){
+
+    Swal.fire({
+        icon: 'warning',
+        title: 'Hallo sayangkuu',
+        text: 'HAPPY SWEET 17 DAY YAA SAYAAANGKUU'
+        
+      }) .then((result) =>{
+        //jawab jujur
+        if(result.isConfirmed){
+            Swal.fire({
+              icon: 'question',
+              title: 'ngga kerasa yaa sayaangkuu yang cummil sdh 17 tahun'
+            }).then((result) =>{
+                //jawab jujur
+                if(result.isConfirmed){
+                    Swal.fire({
+                      icon: 'question',
+                      title: 'tahun yang sudah boleh bikin bikin :)'
+                    }).then((result) =>{
+                        //jawab jujur
+                        if(result.isConfirmed){
+                            Swal.fire({
+                              icon: 'question',
+                              title: 'makasih yaa sayangkuu sdh setia sama kk, sdh sabar sama, kk sdh mau neruma kk apa adanya'
+                            }).then((result) =>{
+                                //jawab jujur
+                                if(result.isConfirmed){
+                                    Swal.fire({
+                                      icon: 'question',
+                                      title: 'kk minta maaf yaa sayangkuu kk sering bikin sayangkuu kesel'
+                                    }).then((result) =>{
+                                        //jawab jujur
+                                        if(result.isConfirmed){
+                                            Swal.fire({
+                                              icon: 'question',
+                                              title: 'kk minta maaf kk masih belum bisa ngebahagiain sayangkuu'
+                                            }).then((result) =>{
+                                                //jawab jujur
+                                                if(result.isConfirmed){
+                                                    Swal.fire({
+                                                      icon: 'question',
+                                                      title: 'kk janji akan selalu bikin sayangkuu seneng kk juga janji jadi ayah yang baik buat anak kita zayn sama zeline.'
+                                                    }).then((result) =>{
+                                                        //jawab jujur
+                                                        if(result.isConfirmed){
+                                                            Swal.fire({
+                                                              icon: 'question',
+                                                              title: 'sayaangkuu selalu sama kk yaaaa. sayangkuu jangan pergi dari kk yaa sayaangkuu ngga boleh sama siap siapa selain kk'
+                                                            }).then((result) =>{
+                                                                //jawab jujur
+                                                                if(result.isConfirmed){
+                                                                    Swal.fire({
+                                                                      icon: 'question',
+                                                                      title: 'di hari yang spesial ini semoga sayaangkuu sehat teruuus semoga kecapai semua impiannya keterima di ugm, selalu jadi kebanggaan keluarga sayangkuu dan kita, selalu kuat menghadapi semua ujian,'
+                                                                    }).then((result) =>{
+                                                                        //jawab jujur
+                                                                        if(result.isConfirmed){
+                                                                            Swal.fire({
+                                                                              icon: 'question',
+                                                                              title: 'SEMOGA SELALU MAKIN SAYANG SAMA KK, SEMOGA BISA JADI IBU YANG BAIK BUAT ANAK KITAA :)'
+                                                                            }).then((result) =>{
+                                                                                //jawab jujur
+                                                                                if(result.isConfirmed){
+                                                                                    Swal.fire({
+                                                                                      icon: 'question',
+                                                                                      title: 'KK SAAYYAAAAANG BANGET SAMA SAYANGKUU SAAYYAAAAAAAAANG BANGET SSAAAYYAAAAAANG BANGET SAMA SAYANGKUU '
+                                                                                    }).then((result) =>{
+                                                                                        //jawab jujur
+                                                                                        if(result.isConfirmed){
+                                                                                            Swal.fire({
+                                                                                              icon: 'question',
+                                                                                              title: 'DI JAWAB YAAAA SAYANGKUU'
+                                                                                            }).then((result) =>{
+                                                                                                //jawab jujur
+                                                                                                if(result.isConfirmed){
+                                                                                                    Swal.fire({
+                                                                                                      icon: 'question',
+                                                                                                      title: 'LOVE YOU FOREVER MY DEAR, FOREVER FOREVER FOREVER :)'
+                                                                                                    }).then((result)=>{
+                                                                                                        if(result.isConfirmed){
+                                                                                                            Swal.fire({
+                                                                                                                title:'coba teken tombol biru :)'
+                                                                                                            })
+                                                                                                            $('#tombol-cari').show();
+                        
+                                                                                                            $('#tombol-cari').on('click',function(){
+                                                                                                                $('#tombol-cari').hide();
+                                                                                                    
+                                                                                                                $('#vidio').show();
+                                                                                                            })
+                                                                                                            
+                                                                                                        }
+                                                                                                    })
+                                                                                                }
+                                                                                            })    
+                                                                                        }
+                                                                                    })    
+                                                                                }
+                                                                            })    
+                                                                        }
+                                                                    })    
+                                                                }
+                                                            })    
+                                                        }
+                                                    })    
+                                                }
+                                            })    
+                                        }
+                                    })    
+                                }
+                            })    
+                        }
+                    })    
+                }
+            })
+        }   
+     })
+    
+
+})
